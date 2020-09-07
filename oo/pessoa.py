a8ba29752e3a6cb6586a1d5a7e82ebcd97814536
dhanniel83 <65819834+dhanniel83@users.noreply.github.com>
2020-09-07 10:40:39 -0300

    Criads atributos de instancia e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,17 @@
 class Pessoa:
+    def __init__(self, nome = None, idade =35):
+        self.nome = nome
+
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Daniel')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Renzo'
+    print(p.nome)
